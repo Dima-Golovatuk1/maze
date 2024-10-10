3934ac4692a2b8d56a3a6588da6a984c5c44e51b
Dima-Golovatuk1 <146225071+Dima-Golovatuk1@users.noreply.github.com>
2024-10-10 18:19:29 +0300

    updata

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import pygame
 import numpy as np
-from function import search, is_valid, print_maze
+from function import search, is_valid, print_maze, hover_button
 
 pygame.init()
 font = pygame.font.Font("../maze/fonts/Roboto-Regular.ttf", 15)
@@ -12,27 +12,30 @@ tile = 50
 cols = width // tile
 rows = height // tile
 
-def hover_button(button, pos):
-    if button.collidepoint(pos):
-        return (0, 255, 0)
-    else:
-        return (0, 128, 0)
-
 text_start = font.render('start', False, 'White')
 button_start_rect = pygame.Rect(width - 200, height - 49, 200, 49)
+start_x = 0
+start_y = 0
+end = [13, 13]
 
 class Cell:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.wall = False
+        self.wall = 0
 
     def draw(self):
         x = self.x * tile
         y = self.y * tile
         wall = self.wall
-        if not wall:
+        if wall == 0:
             pygame.draw.rect(screen, (255, 255, 255), (x, y, tile, tile))
+        elif wall == 2:
+            pygame.draw.rect(screen, (0, 255, 0), (x, y, tile, tile))
+        elif wall == 4:
+            pygame.draw.rect(screen, (0, 128, 0), (x, y, tile, tile))
+        elif wall == 3:
+            pygame.draw.rect(screen, (0, 255, 127), (x, y, tile, tile))
         else:
             pygame.draw.rect(screen, (128, 128, 128), (x, y, tile, tile))
         pygame.draw.rect(screen, (0, 0, 0), (x, y, tile, tile), 1)
@@ -53,11 +56,31 @@ for row in range(rows):
 def build_matrix(grid):
     matrix = np.zeros((rows, cols))
     for cell in grid:
-        if cell.wall:
+        if cell.wall == 1:
             matrix[cell.y][cell.x] = 0
+        elif cell.wall == 3:
+            matrix[cell.y][cell.x] = 3
+        elif cell.wall == 4:
+            matrix[cell.y][cell.x] = 4
         else:
             matrix[cell.y][cell.x] = 1
     return matrix
+
+def build_matrix_new(maze, grid):
+    for y in range(len(maze)):
+        for x in range(len(maze[y])):
+            cell = grid[y * cols + x]
+            if maze[y][x] == 1:
+                cell.wall = 0
+            elif maze[y][x] == 0:
+                cell.wall = 1
+            elif maze[y][x] == 2:
+                cell.wall = 2
+            elif maze[y][x] == 3:
+                cell.wall = 3
+            elif maze[y][x] == 4:
+                cell.wall = 4
+
 
 
 matrix = build_matrix(grid)
@@ -82,22 +105,33 @@ while running:
             if event.button == 1:
                 for cell in grid:
                     if cell.is_mouse_over(pos):
-                        cell.wall = True
+                        cell.wall = 1
             elif event.button == 3:
                 for cell in grid:
                     if cell.is_mouse_over(pos):
-                        cell.wall = False
+                        cell.wall = 0
             if button_start_rect.collidepoint(pos):
                 matrix = build_matrix(grid)
                 result = []
-                n = len(matrix)
-                row_index = 0
-                col_index = 0
-                mouse = int(matrix[row_index, col_index])
-                cheese = int(matrix[13, 13])
                 print(matrix)
-                search(matrix, 0, 0, result)
-                print_maze(matrix, result)
-
+                search(matrix, start_y, start_x, result, end)
+                maze = print_maze(matrix, result, start_y, start_x, end)
+                build_matrix_new(maze, grid)
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_q:
+                for cell in grid:
+                    if cell.is_mouse_over(pos):
+                        cell.wall = 2
+                        start_x = cell.x
+                        start_y = cell.y
+                        build_matrix(grid)
+                        print(start_x, start_y)
+            elif event.key == pygame.K_w:
+                for cell in grid:
+                    if cell.is_mouse_over(pos):
+                        cell.wall = 4
+                        end = [cell.y, cell.x]
+                        build_matrix(grid)
+                        print(start_x, start_y)
 
 pygame.quit()
--- function.py
+++ function.py
@@ -1,31 +1,47 @@
-def is_valid(row, col, maze):
-    return 0 <= row < len(maze) and 0 <= col < len(maze[0]) and maze[row][col] == 1
+import random
+def is_valid(x, y, maze):
+    return 0 <= x < len(maze[1]) and 0 <= y < len(maze[0]) and maze[x][y] == 1
 
 
-def search(maze, x, y, result):
-    go = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-    if x == len(maze) - 1 and y == len(maze[0]) - 1:
+def search(maze, x=0, y=0, result=None, end=[13, 13]):
+    if x == end[0] and y == end[1]:
         result.append([x, y])
-        print(1532)
-        maze[x, y] = 4
+        maze[x][y] = 4
+        maze[result[0][0]][result[0][1]] = 3
         return True
     if is_valid(x, y, maze):
         result.append([x, y])
         maze[x][y] = 2
-        for walk in go:
-            x += walk[0]
-            y += walk[1]
-            if search(maze, x, y, result):
-                return True
+        if search(maze, x + 1, y, result, end):
+            return True
+        if search(maze, x, y + 1, result, end):
+            return True
+        if search(maze, x - 1, y,  result, end):
+            return True
+        if search(maze, x, y - 1, result, end):
+            return True
         result.pop()
-        maze[x][y] = 0
-    return False
+        maze[x][y] = 9
+    else: return False
 
 
-def print_maze(maze, result):
-    if search(maze, 0, 0, result):
-        print("Найден путь:", result)
+def print_maze(maze, result, start_x=0, start_y=0, end=[13, 13]):
+    if search(maze, start_x, start_y, result, end):
+        print("Вихід знайдено:", result)
     else:
-        print("Выхода нет", result)
-    for i in maze:
-        print(i)
+        print("Вихід не знайдено")
+
+    for row in maze:
+        print(row)
+    return maze
+
+
+def hover_button(button, pos):
+    if button.collidepoint(pos):
+        return (0, 255, 0)
+    else:
+        return (0, 128, 0)
+
+
+
+
